09bc183859b4765e38bd827692e646bfd2ea4fa0
John Tsiombikas <nuclear@member.fsf.org>
2020-09-26 15:16:01 +0300

    video plugin

--- plugins/video/main.c
+++ plugins/video/main.c
@@ -1,8 +1,26 @@
+/*
+xlivebg - live wallpapers for the X window system
+Copyright (C) 2019-2020  John Tsiombikas <nuclear@member.fsf.org>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <GL/gl.h>
 #include "xlivebg.h"
+#include "vid.h"
 
 static int init(void *cls);
 static void start(long tmsec, void *cls);
@@ -10,6 +28,7 @@ static void stop(void *cls);
 static void prop(const char *prop, void *cls);
 static void draw(long tmsec, void *cls);
 static void draw_static(long tmsec, float aspect);
+static unsigned int nextpow2(unsigned int x);
 
 #define PROPLIST	\
 	"proplist {\n" \
@@ -24,7 +43,7 @@ static struct xlivebg_plugin plugin = {
 	"video",
 	"Video playback",
 	PROPLIST,
-	XLIVEBG_25FPS,
+	XLIVEBG_15FPS,
 	init, 0,
 	start, stop,
 	draw,
@@ -32,8 +51,11 @@ static struct xlivebg_plugin plugin = {
 	0, 0
 };
 
-static const char *vidfile;
-static unsigned int static_tex;
+static struct video_file *vidfile;
+static int vid_width, vid_height, tex_width, tex_height;
+static unsigned int vid_tex, static_tex;
+static unsigned long interval;
+static uint32_t *framebuf;
 #define STATIC_SZ	128
 
 int register_plugin(void)
@@ -49,8 +71,6 @@ static int init(void *cls)
 
 static void start(long tmsec, void *cls)
 {
-	prop("video", 0);
-
 	glGenTextures(1, &static_tex);
 	glBindTexture(GL_TEXTURE_2D, static_tex);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
@@ -59,24 +79,116 @@ static void start(long tmsec, void *cls)
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_LUMINANCE, STATIC_SZ, STATIC_SZ, 0,
 			GL_LUMINANCE, GL_UNSIGNED_BYTE, 0);
+
+	glGenTextures(1, &vid_tex);
+	glBindTexture(GL_TEXTURE_2D, vid_tex);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+	prop("video", 0);
 }
 
 static void stop(void *cls)
 {
-	glDeleteTextures(1, &static_tex);
+	if(static_tex) {
+		glDeleteTextures(1, &static_tex);
+	}
+	if(vid_tex) {
+		glDeleteTextures(1, &vid_tex);
+	}
+
+	if(vidfile) {
+		vid_close(vidfile);
+		vidfile = 0;
+	}
+	free(framebuf);
+	framebuf = 0;
+
+	plugin.upd_interval = XLIVEBG_15FPS;
 }
 
 static void prop(const char *prop, void *cls)
 {
 	if(strcmp(prop, "video") == 0) {
-		vidfile = xlivebg_getcfg_str("xlivebg.video.video", 0);
+		int tw, th;
+		struct video_file *vf;
+		uint32_t *fb;
+		const char *fname = xlivebg_getcfg_str("xlivebg.video.video", 0);
+
+		if(!fname || !*fname || !(vf = vid_open(fname))) {
+			return;
+		}
+
+		if(!(fb = malloc(vid_frame_size(vf)))) {
+			fprintf(stderr, "video: failed to allocate frame buffer %u bytes\n", (unsigned int)vid_frame_size(vf));
+			vid_close(vf);
+			return;
+		}
+		free(framebuf);
+		framebuf = fb;
+
+		if(vidfile) vid_close(vidfile);
+		vidfile = vf;
+
+		plugin.upd_interval = vid_frame_interval(vidfile);
+
+		vid_width = vid_frame_width(vidfile);
+		vid_height = vid_frame_height(vidfile);
+		tw = nextpow2(vid_width);
+		th = nextpow2(vid_height);
+
+		if(tex_width < tw || tex_height < th) {
+			tex_width = tw;
+			tex_height = th;
+
+			glBindTexture(GL_TEXTURE_2D, vid_tex);
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, tex_width, tex_height, 0, GL_RGBA,
+					GL_UNSIGNED_BYTE, 0);
+		}
+		glMatrixMode(GL_TEXTURE);
+		glLoadIdentity();
+		glScalef((float)vid_width / tex_width, (float)vid_height / tex_height, 1);
+
+		interval = 0;
 	}
+}
+
+static void update_frame(unsigned long dt)
+{
+	int count = 0;
+
+	interval += dt * 1000;
+
+	while(interval >= plugin.upd_interval) {
+		if(vid_get_frame(vidfile, framebuf) == -1) break;
+		count++;
+		interval -= plugin.upd_interval;
+	}
+	if(!count) return;
+
+	glBindTexture(GL_TEXTURE_2D, vid_tex);
+	glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, vid_width, vid_height, GL_BGRA,
+			GL_UNSIGNED_BYTE, framebuf);
 }
 
 static void draw(long tmsec, void *cls)
 {
 	int i, num_scr;
 	struct xlivebg_screen *scr;
+	float xform[16];
+	static long prev_tmsec, dt;
+
+	dt = tmsec - prev_tmsec;
+	prev_tmsec = tmsec;
+
+	if(vidfile) {
+		update_frame(dt);
+	} else {
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+	}
 
 	xlivebg_clear(GL_COLOR_BUFFER_BIT);
 
@@ -85,8 +197,28 @@ static void draw(long tmsec, void *cls)
 		xlivebg_gl_viewport(i);
 		scr = xlivebg_screen(i);
 
-		/* TODO */
-		draw_static(tmsec, scr->aspect);
+		if(vidfile) {
+			xlivebg_calc_image_proj(i, (float)vid_width / (float)vid_height, xform);
+			glMatrixMode(GL_PROJECTION);
+			glLoadMatrixf(xform);
+
+			glEnable(GL_TEXTURE_2D);
+			glBindTexture(GL_TEXTURE_2D, vid_tex);
+
+			glBegin(GL_QUADS);
+			glTexCoord2f(0, 1);
+			glVertex2f(-1, -1);
+			glTexCoord2f(1, 1);
+			glVertex2f(1, -1);
+			glTexCoord2f(1, 0);
+			glVertex2f(1, 1);
+			glTexCoord2f(0, 0);
+			glVertex2f(-1, 1);
+			glEnd();
+
+		} else {
+			draw_static(tmsec, scr->aspect);
+		}
 	}
 }
 
@@ -123,4 +255,15 @@ static void draw_static(long tmsec, float aspect)
 	glEnd();
 
 	glDisable(GL_TEXTURE_2D);
+}
+
+static unsigned int nextpow2(unsigned int x)
+{
+	x--;
+	x |= x >> 1;
+	x |= x >> 2;
+	x |= x >> 4;
+	x |= x >> 8;
+	x |= x >> 16;
+	return x + 1;
 }
---
+++ plugins/video/vid.c
@@ -0,0 +1,184 @@
+/*
+xlivebg - live wallpapers for the X window system
+Copyright (C) 2019-2020  John Tsiombikas <nuclear@member.fsf.org>
+
+Stereoplay - an OpenGL stereoscopic video player.
+Copyright (C) 2011  John Tsiombikas <nuclear@member.fsf.org>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+#include <stdio.h>
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+#include <libavutil/imgutils.h>
+#include "vid.h"
+
+struct video_file {
+	AVFormatContext *avctx;
+	AVCodecContext *cctx;
+	AVCodec *codec;
+	int vstream, audio_stream;
+	struct SwsContext *sws;
+
+	AVFrame *frm, *rgbfrm;
+};
+
+struct video_file *vid_open(const char *fname)
+{
+	static int initialized;
+	struct video_file *vf;
+	int i;
+
+	if(!initialized) {
+		av_register_all();
+		initialized = 1;
+	}
+
+	if(!(vf = malloc(sizeof *vf))) {
+		fprintf(stderr, "open_video(%s): failed to allocate memory: %s\n", fname, strerror(errno));
+		return 0;
+	}
+	memset(vf, 0, sizeof *vf);
+
+	if(avformat_open_input(&vf->avctx, fname, 0, 0) != 0) {
+		fprintf(stderr, "open_video(%s): failed to open file\n", fname);
+		vid_close(vf);
+		return 0;
+	}
+
+	if(avformat_find_stream_info(vf->avctx, 0) < 0) {
+		fprintf(stderr, "open_video(%s): failed to find stream info\n", fname);
+		vid_close(vf);
+		return 0;
+	}
+
+	vf->vstream = -1;
+	for(i=0; i<vf->avctx->nb_streams; i++) {
+		if(vf->avctx->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO) {
+			vf->vstream = i;
+			break;
+		}
+	}
+	if(vf->vstream == -1) {
+		fprintf(stderr, "open_video(%s): didn't find a video stream\n", fname);
+		vid_close(vf);
+		return 0;
+	}
+	vf->cctx = vf->avctx->streams[vf->vstream]->codec;
+
+	if(!(vf->codec = avcodec_find_decoder(vf->cctx->codec_id))) {
+		fprintf(stderr, "open_video(%s): unsupported codec\n", fname);
+		vid_close(vf);
+		return 0;
+	}
+
+	if(avcodec_open2(vf->cctx, vf->codec, 0) < 0) {
+		fprintf(stderr, "open_video(%s): failed to open codec\n", fname);
+		vid_close(vf);
+		return 0;
+	}
+
+	if(!(vf->frm = av_frame_alloc()) || !(vf->rgbfrm = av_frame_alloc())) {
+		fprintf(stderr, "open_video(%s): failed to allocate frame\n", fname);
+		vid_close(vf);
+		return 0;
+	}
+
+	vf->sws = sws_getContext(vf->cctx->width, vf->cctx->height, vf->cctx->pix_fmt,
+			vf->cctx->width, vf->cctx->height, AV_PIX_FMT_RGB32, SWS_POINT, 0, 0, 0);
+	if(!vf->sws) {
+		fprintf(stderr, "open_video(%s): failed to allocate sws context\n", fname);
+		vid_close(vf);
+		return 0;
+	}
+
+
+	printf("using codec: %s\n", vf->codec->name);
+	printf("fps: %f (%u usec frame interval)\n", vid_fps(vf), vid_frame_interval(vf));
+	printf("size: %dx%d\n", vid_frame_width(vf), vid_frame_height(vf));
+
+	return vf;
+}
+
+void vid_close(struct video_file *vf)
+{
+	if(!vf) return;
+
+	/* TODO how do we deallocate sws contexts? */
+	if(vf->rgbfrm) av_free(vf->rgbfrm);
+	if(vf->frm) av_free(vf->frm);
+	if(vf->cctx) avcodec_close(vf->cctx);
+	if(vf->avctx) avformat_close_input(&vf->avctx);
+	free(vf);
+}
+
+
+float vid_fps(struct video_file *vf)
+{
+	float inv_tb = (float)vf->cctx->time_base.den / (float)vf->cctx->time_base.num;
+	return inv_tb / (float)vf->cctx->ticks_per_frame;
+}
+
+unsigned int vid_frame_interval(struct video_file *vf)
+{
+	float fps = vid_fps(vf);
+	return 1000000 / fps;
+}
+
+int vid_frame_width(struct video_file *vf)
+{
+	return vf->cctx->width;
+}
+
+int vid_frame_height(struct video_file *vf)
+{
+	return vf->cctx->height;
+}
+
+size_t vid_frame_size(struct video_file *vf)
+{
+	return vf->cctx->width * vf->cctx->height * 4;
+}
+
+int vid_get_frame(struct video_file *vf, uint32_t *img)
+{
+	AVPacket packet;
+	int res, frame_done = 0;
+
+	av_image_fill_arrays(vf->rgbfrm->data, vf->rgbfrm->linesize, (unsigned char*)img,
+			AV_PIX_FMT_RGB32, vf->cctx->width, vf->cctx->height, 1);
+
+retry:
+	while((res = av_read_frame(vf->avctx, &packet)) >= 0) {
+		if(packet.stream_index == vf->vstream) {
+			avcodec_decode_video2(vf->cctx, vf->frm, &frame_done, &packet);
+
+			if(frame_done) {
+				sws_scale(vf->sws, vf->frm->data, vf->frm->linesize, 0, vf->cctx->height,
+						vf->rgbfrm->data, vf->rgbfrm->linesize);
+				av_packet_unref(&packet);
+				return 0;
+			}
+		}
+		av_packet_unref(&packet);
+	}
+
+	if(res == AVERROR_EOF) {
+		av_seek_frame(vf->avctx, vf->vstream, 0, AVSEEK_FLAG_BYTE);
+		goto retry;
+	}
+
+	return -1;
+}
